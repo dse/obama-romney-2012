893cd2892678697b48c38e67b5e016ba75eb4d09
Darren Embry <dse@webonastick.com>
2012-11-06 19:28:19 -0500

    comments and such

--- stats.pl
+++ stats.pl
@@ -2,20 +2,24 @@
 use warnings;
 use strict;
 
+# if true, each simulation is shortcutted after one party gets 270,
+# and we don't get to compute likely electoral vote totals.
 my $quick = 0;
+
+# number of simulations to run.
 my $runs = 100000;
 
-my @swing;
-my %swing;
-my @state;
-my %votes;
-my %status;
-my %dem;
-my %rep;
-my $totalvotes;
+my @swing;			# list of swing states
+my %swing;			# whether each state is a swing state
+my @state;			# list of states
+my %votes;			# electoral votes by state
+my %status;			# 'status' field of state: lc is 'swing' indicates swing
+my %dem;			# probability that dem wins each state
+my %rep;			# probability that rep wins each state
+my $totalvotes;			# total number of electoral votes
 while (<>) {
 	chomp();
-	next if /^\s*\#/;
+	next if /^\s*\#/;	# skip comments
 	my ($state, $votes, $status, $dem, $rep) = split(/\t/);
 	push(@state, $state);
 	$votes{$state} = $votes;
@@ -31,39 +35,59 @@ while (<>) {
 	}
 	$totalvotes += $votes;
 	if (abs($dem + $rep - 1) > 0.00001) {
-		die("Dem + Rep != 1:\t$_\n");
+		die("OH SHIT: Dem + Rep != 1:\t$_\n");
 	}
 }
+
 printf("Total electoral votes: %d\n", $totalvotes);
 
-my $demwins = 0;
-my $repwins = 0;
-my $ties = 0;
-my @votecountdist = map { 0 } (0 .. $totalvotes);
-my %demvotes = map { ($_, 0) } @state;
-my %repvotes = map { ($_, 0) } @state;
+my $demwins = 0;		# number of dem wins over all simulations
+my $repwins = 0;		# "      "  rep "    "    "   "
+my $ties = 0;			# number of ties     "    "   "
 
+my @votecountdist = map { 0 } (0 .. $totalvotes); # for computing likeliest vote counts
+
+# total number of electoral votes each party receives in each state,
+# over all simulations
+my $totaldemvotes = 0;
+my $totalrepvotes = 0;
+my %demvotes = map { ($_, 0) } @state;		  
+my %repvotes = map { ($_, 0) } @state;		  
+
+# sort states primarily by swinginess and secondarily by descending
+# number of electoral votes
 @state = sort { $swing{$b} <=> $swing{$a} || $votes{$b} <=> $votes{$a} } @state;
 
-my $demvotes;
-my $repvotes;
+my $demvotes;			# number of dem votes each sim
+my $repvotes;			# "      "  rep "     "    "
 sub simulate {
-	my $quick = $quick;
 	my ($run_number) = @_;
+
+	my $quick = $quick;
 	if ($run_number) {
 		$quick = 0;	# so we can show final totals on these runs
 	}
-	$demvotes = 0;
-	$repvotes = 0;
+
+	$demvotes = 0;		# total number of dem votes this simulation run
+	$repvotes = 0;		# "     "      "  rep "     "    "          "
 	foreach my $state (@state) {
 		if (rand() < $dem{$state}) {
+			# this run only
 			$demvotes += $votes{$state};
+
+			# over all simulation runs
+			$totaldemvotes += $votes{$state};
 			$demvotes{$state} += $votes{$state};
 		} else {
+			# this run only
 			$repvotes += $votes{$state};
+
+			# over all simulation runs
+			$totalrepvotes += $votes{$state};
 			$repvotes{$state} += $votes{$state};
 		}
 		if ($quick) {
+			# shortcut this simulation run maybe
 			if ($demvotes > ($totalvotes / 2)) {
 				last;
 			} elsif ($repvotes > ($totalvotes / 2)) {
@@ -79,11 +103,13 @@ sub simulate {
 		$demwins += 1;
 	}
 	if ($run_number) {
-		printf("Run %8d: Dem %3d Rep %3d\n", $run_number, $demvotes, $repvotes);
+		printf("Run %8d: Dem %3d Rep %3d\n",
+		       $run_number, $demvotes, $repvotes);
 	}
 	$votecountdist[$demvotes] += 1;
 }
 
+# Run the simulations.
 for (my $run = 1; $run <= $runs; $run += 1) {
 	if ($run % 10000 == 0) {
 		simulate($run);
@@ -91,23 +117,30 @@ for (my $run = 1; $run <= $runs; $run += 1) {
 		simulate();
 	}
 }
+
+# Show simulation results.
 printf STDERR ("Done!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
-printf STDERR ("  Dems are %7.3f%% likely to win.\n",
-	       100 * $demwins / $runs);
-printf STDERR ("  Reps are %7.3f%% likely to win.\n",
-	       100 * $repwins / $runs);
-printf STDERR ("  A tie is %7.3f%% likely.\n",
-	       100 * $ties / $runs);
+print ("Results\n");
+printf ("  Dems are %7.3f%% likely to win.\n", 100 * $demwins / $runs);
+printf ("  Reps are %7.3f%% likely to win.\n", 100 * $repwins / $runs);
+printf ("  A tie is %7.3f%% likely.\n",        100 * $ties / $runs);
+
+print ("Average electoral vote counts\n");
+printf ("  %6.2 Dem %6.2 Rep\n",
+	$totaldemvotes / $runs, $totalrepvotes / $runs);
 
 exit 0 if $quick;
 
+# sort (number of dem electoral votes) scenarios from most to least
+# likely
 my @likelydemtotals = sort { $votecountdist[$b] <=> $votecountdist[$a] }
 	(0 .. $totalvotes);
 
 print STDERR ("Top 20 likely scenarios...\n");
-foreach my $demtotal (@likelydemtotals[0..19]) {
-	printf("  Dem %3d Rep %3d %7.3f%%\n",
-	       $demtotal, $totalvotes - $demtotal,
+foreach my $i (0..19) {
+	my $demtotal = $likelydemtotals[$i];
+	printf("  %.2f.  Dem %3d Rep %3d %7.3f%%\n",
+	       $i + 1, $demtotal, $totalvotes - $demtotal,
 	       100 * $votecountdist[$demtotal] / $runs);
 }
 
